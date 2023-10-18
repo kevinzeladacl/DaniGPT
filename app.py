d52e8b1749a29ebfeeb856945e8045e880758b85
Kevinzeladacl <iam@kevinzelada.cl>
2023-10-18 01:04:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import streamlit as st
-import time
-import requests
-import json
-import os
-import asyncio
 from PIL import Image
 from judini.codegpt.agent import Agent
-from dotenv import load_dotenv
-load_dotenv()
+import asyncio
+import time
 
 st.set_page_config(layout="centered")  
 
@@ -19,37 +14,58 @@ with col1:
 
 with col2:
     st.title("DaniGPT 🤖")
-st.write("Preguntame cualquier cosa sobre python, Judini o Langchain")
+
+
+st.write("Pregúntame cualquier cosa sobre Python, Judini o Langchain")
 st.write("OJO 👀 No estoy conectado a internet, así que puedo alucinar 😵‍💫, es tu trabajo corroborar que la respuesta que te entregue sea correcta.")
 st.markdown('---')
+
 # Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+	st.session_state.messages = []
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+	with st.chat_message(message["role"]):
+		st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("En que te puedo ayudar?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if prompt := st.chat_input("En qué te puedo ayudar?"):
+	# Add user message to chat history
+	st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        #Judini
-        codegpt_api_key= st.secrets["CODEGPT_API_KEY"]
-        codegpt_agent_id= st.secrets["CODEGPT_AGENT_ID"]
+	# Display user message in chat message container
+	with st.chat_message("user"):
+		st.markdown(prompt)
 
-        agent_instance = Agent(api_key=codegpt_api_key, agent_id=codegpt_agent_id)
+	# Display assistant response in chat message container
+	with st.chat_message("assistant"):
+		message_placeholder = st.empty()
 
-        full_response = asyncio.run(agent_instance.completion(prompt, stream=False))
-        
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+		# Utilizar el módulo judini.codegpt.agent para obtener la respuesta
+		async def get_assistant_response(prompt):
+			CODEGPT_API_KEY = "XXXXX"
+			CODEGPT_AGENT_ID = "XXXXX"
+
+			agent_instance = Agent(api_key=CODEGPT_API_KEY, agent_id=CODEGPT_AGENT_ID)
+			# Utilizar el método chat_completion del agente para obtener la respuesta
+			response_text = ""
+			
+			print(st.session_state.messages)
+			async for response_chunk in agent_instance.chat_completion(st.session_state.messages, stream=False):
+				response_chunk_text = response_chunk.strip()  # Obtener el texto del fragmento
+
+				for chunk in response_chunk_text.split():
+					response_text += chunk
+					message_placeholder.markdown(response_text)
+					time.sleep(0.09)
+				
+
+				# response_text += response_chunk_text
+				# message_placeholder.markdown(response_chunk_text)
+
+			# Agregar el contenido al diccionario de mensajes
+			
+			st.session_state.messages.append({"role": "assistant", "content": response_text})
+
+		asyncio.run(get_assistant_response(prompt))
